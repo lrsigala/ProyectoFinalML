5ad1bf48473d556cd4da6ef2c71c01b6c5e913b7
Luis Raúl Sigala <143978553+lrsigala@users.noreply.github.com>
2024-11-20 20:06:19 -0600

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def load_model(file_path):
 
 # Rutas a los archivos de modelos
 model_paths = {
-    #"Modelo 1: RandomForest": "rf_pipeline.pkl",
+    "Modelo 1: RandomForest": "rf_pipeline.pkl",
     "Modelo 2: SVM": "svm_pipeline.pkl",
     #"Modelo 3: Voting Ensamble": "vot_pipeline.pkl",
 }
@@ -77,7 +77,7 @@ selected_model = models[selected_model_name]
 
 # Mostrar imágenes asociadas al modelo seleccionado
 image_paths = {
-    #"Modelo 1: RandomForest": ("assets/rfcr.jpg", "assets/rfcm.jpg"),
+    "Modelo 1: RandomForest": ("assets/rfcr.jpg", "assets/rfcm.jpg"),
     "Modelo 2: SVM": ("assets/svmcr.jpg", "assets/svmcm.jpg"),
     #"Modelo 3: Voting Ensamble": ("assets/vocr.jpg", "assets/vocm.jpg"),
 }
